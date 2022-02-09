9bf8105086ed23765badb4ad547ff4b5152548fb
Viktoria Vasyutinskaya <solovyanova.v@gmail.com>
2022-02-09 20:25:43 +0100

    added function for temperature and date,time
    time function including adjustments to timezone

--- source/app.js
+++ source/app.js
@@ -1,1 +1,49 @@
-alert("hello");
+function setDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `Last updated: ${day} ${hours}:${minutes}`;
+}
+
+function showTemp(response) {
+  console.log(response.data);
+  let tempElem = document.querySelector("#currentTemp");
+  tempElem.innerHTML = Math.round(response.data.main.temp);
+  let date = document.querySelector("#date");
+  date.innerHTML = setDate(
+    response.data.dt * 1000 - response.data.timezone * 1000
+  );
+  let cityElem = document.querySelector("#city");
+  cityElem.innerHTML = response.data.name;
+  let conditions = document.querySelector("#conditions");
+  conditions.innerHTML = response.data.weather[0].description;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = `Wind: ${response.data.wind.speed} km/h`;
+  let feelsLike = document.querySelector("#feel");
+  feelsLike.innerHTML = `Feels like: ${Math.round(
+    response.data.main.feels_like
+  )}°`;
+}
+
+let apiKey = "8dc5c84de9b99758c12092b7cd18ffae";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(`${apiUrl}`).then(showTemp);
