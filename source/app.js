6499b5fbe95e3db02d98f4c554bca55bbb167ee1
Viktoria Vasyutinskaya <solovyanova.v@gmail.com>
2022-02-10 10:48:57 +0100

    added function for search form, search city and form submit

--- source/app.js
+++ source/app.js
@@ -24,7 +24,7 @@ function setDate(timestamp) {
 function showTemp(response) {
   console.log(response.data);
   let tempElem = document.querySelector("#currentTemp");
-  tempElem.innerHTML = Math.round(response.data.main.temp);
+  tempElem.innerHTML = `${Math.round(response.data.main.temp)}°`;
   let date = document.querySelector("#date");
   date.innerHTML = setDate(
     response.data.dt * 1000 - response.data.timezone * 1000
@@ -47,8 +47,19 @@ function showTemp(response) {
   console.log(response.data.weather[0].icon);
 }
 
-let apiKey = "8dc5c84de9b99758c12092b7cd18ffae";
-let city = "Moscow";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
-axios.get(`${apiUrl}`).then(showTemp);
+function search(city) {
+  let apiKey = "8dc5c84de9b99758c12092b7cd18ffae";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(`${apiUrl}`).then(showTemp);
+}
+
+function submitSearch(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector(".type-city");
+  search(cityInput.value);
+}
+
+search("Moscow");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", submitSearch);
